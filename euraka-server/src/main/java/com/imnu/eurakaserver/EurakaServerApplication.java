6cf7e4f09a3811811f91535a6024d1b1996b7715
20161104585 <1663512557@qq.com>
2021-01-11 17:01:20 +0800

    Initial commit

---
+++ euraka-server/src/main/java/com/imnu/eurakaserver/EurakaServerApplication.java
@@ -0,0 +1,15 @@
+package com.imnu.eurakaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurakaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurakaServerApplication.class, args);
+    }
+
+}
---
+++ euraka-server/src/test/java/com/imnu/eurakaserver/EurakaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imnu.eurakaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurakaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
